2c2bdb5bc9694fe841861e03bdf1ac90cc3df675
Borges dev <leosull50@hotmail.com>
2025-02-11 23:37:37 -0300

    JWT auth desacoplado

---
+++ JWT_SOLID_LIVE_006/src/server/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { makeSignUpController } from "../factories/makeSignUpController";
+import { makeSignInController } from "../factories/makeSignInController";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/sign-up", async (request, response) => {
+  const signUpController = makeSignUpController();
+
+  const { statusCode, body } = await signUpController.handle({
+    body: request.body,
+  });
+
+  response.status(statusCode).json(body);
+});
+
+app.post("/sign-in", async (request, response) => {
+  const signInController = makeSignInController();
+
+  const { statusCode, body } = await signInController.handle({
+    body: request.body,
+  });
+  response.status(statusCode).json(body);
+});
+
+app.listen(3001, () => {
+  console.log("server started at http://localhost:3001");
+});
