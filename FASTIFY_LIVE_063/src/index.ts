b4c11d98436857ad1d5f09ed9bb056e1f4299aef
Borges dev <leosull50@hotmail.com>
2025-06-07 20:03:57 -0300

    iniciando com fastify

---
+++ FASTIFY_LIVE_063/src/index.ts
@@ -0,0 +1,40 @@
+import Fastify, { type FastifyRequest } from "fastify";
+
+const fastify = Fastify();
+
+type Request = FastifyRequest<{
+  Params: { id: string };
+  Querystring: { page: string };
+  Body: { name: string };
+  Headers: { org: string };
+}>;
+
+fastify.post("/users/:id", (request: Request, reply) => {
+  const { params, body, query, headers } = request;
+
+  reply.code(201).send({
+    params: {
+      id: params.id,
+    },
+    query: {
+      page: Number(query.page),
+    },
+    body: {
+      name: body.name,
+    },
+    headers: {
+      org: headers.org,
+    },
+  });
+});
+
+async function main() {
+  try {
+    const host = await fastify.listen({ port: 3000 });
+    console.log(`server started at  ${host}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
