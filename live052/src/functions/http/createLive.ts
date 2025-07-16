6a8acc0f9b113a8c77c46ffde17c5c0a979c878b
Borges dev <leosull50@hotmail.com>
2025-07-16 13:48:46 -0300

    salvando imagem no bucket e dynamodb

---
+++ live052/src/functions/http/createLive.ts
@@ -0,0 +1,57 @@
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { response } from "../../utils/response";
+import z from "zod";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { env } from "../../config/env";
+import { randomUUID } from "node:crypto";
+import { dynamoClient } from "../../clients/dynamoClient";
+import { extractFileInfo } from "../../utils/extractFileInfo";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { s3Client } from "../../clients/s3Client";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+
+const schema = z.object({
+  title: z.string().min(1),
+  number: z.number().min(0),
+  fileName: z.string().min(0),
+});
+
+export async function handler(event: APIGatewayProxyEventV2) {
+  const body = JSON.parse(event.body ?? "{}");
+
+  const { success, data, error } = schema.safeParse(body);
+
+  if (!success) {
+    return response(400, { error: error.issues });
+  }
+
+  const { number, title, fileName } = data;
+  const { extension } = extractFileInfo(fileName);
+
+  const liveId = randomUUID();
+
+  const thumbnailKey = `${randomUUID()}.${extension}`;
+
+  const putItemCommand = new PutCommand({
+    TableName: env.LIVES_TABLE,
+    Item: {
+      id: liveId,
+      number,
+      title,
+      thumbnailKey,
+    },
+  });
+
+  const putObjectCommand = new PutObjectCommand({
+    Bucket: env.LIVES_IMAGES_BUCKET,
+    Key: thumbnailKey,
+  });
+
+  const uploadUrl = await getSignedUrl(s3Client, putObjectCommand, {
+    expiresIn: 600,
+  });
+
+  await dynamoClient.send(putItemCommand);
+
+  return response(201, { liveId, uploadUrl });
+}
